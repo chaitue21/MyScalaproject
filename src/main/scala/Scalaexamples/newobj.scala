82c261582dae84e6f4d131caeef9e4c541b42826
chaitue21 <chaitue21@gmail.com>
2023-03-07 17:39:01 +0530

    First Commit

---
+++ src/main/scala/Scalaexamples/newobj.scala
@@ -0,0 +1,19 @@
+package Scalaexamples
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+object newobj {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder.master("local[*]").appName("newobj").getOrCreate()
+    //    val ssc = new StreamingContext(spark.sparkContext, Seconds(10))
+    val sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
+    import spark.implicits._
+   // import spark.sql
+    val data = "C:\\Users\\Dev12\\Downloads\\drive-dataset\\10000Records.csv"
+    val df = spark.read.format(source = "csv").option("header", "true").option("inferSchema", "true").load(data)
+    df.show()
+    spark.stop()
+  }
+}
